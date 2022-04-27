42d562335e876525ad2eae72ab280726c535575d
Dana Jansens <danakj@orodu.net>
2022-04-26 21:40:42 -0400

    Make the assets directory before calling game_assets to generate stuff there

--- src/bin/make_pdx.rs
+++ src/bin/make_pdx.rs
@@ -5,16 +5,22 @@ fn main() {
 
 #[cfg(feature = "bins")]
 fn main() {
+  let srcdir = env!("PDX_SOURCE_DIR");
+
+  if !std::path::PathBuf::from(srcdir).exists() {
+    if let Err(e) = std::fs::create_dir(srcdir) {
+      println!("Failed making PDX_SOURCE_DIR dir ({}): {}", srcdir, e);
+    }
+  }
+
   // TODO: Generate assets for your game here, copying them to `env!("PDX_SOURCE_DIR")`.
   // Example:
-  //   game_assets::generate_assets(env!("PDX_SOURCE_DIR"))?;
+  //   if let Err(e) = game_assets::generate_assets(srcdir) {
+  //     println!("Failed generating assets\n{}", e);
+  //   }
 
   // Builds the game's pdx image.
-  let r = craydate_build::build_pdx(
-    env!("PDX_SOURCE_DIR"),
-    env!("PDX_OUT_DIR"),
-    env!("PDX_NAME"),
-  );
+  let r = craydate_build::build_pdx(srcdir, env!("PDX_OUT_DIR"), env!("PDX_NAME"));
   match r {
     Ok(stdout) => println!("{}", stdout),
     Err(e) => println!("Failed\n{}", e),
